42e4de37fb66584adf3379476fb719b02087ccae
Kim JeongJin <kjjgo35@gmail.com>
2020-08-18 23:26:09 +0900

    first nodejs_mariadb

---
+++ nodejs/mysql.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
+var connection = mysql.createConnection({
+    host: '34.64.253.187',
+    user: 'kjjgo35',
+    password: '123456',
+    port: '3306',
+    database: 'opentutorials'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM topic', function (error, results, fields) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+});
+
+connection.end();
